0cda0e496ba25d74d44afad550882cea315ea6a2
Bhuvana <bhuvanamjothivel@gmail.com>
2020-02-07 15:01:49 +0530

    Brain Test is done by using Class And Object

---
+++ src/testing/TestPersonalityCalculator.java
@@ -0,0 +1,34 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import service.PersonalityCalculator;
+
+class TestPersonalityCalculator {
+	PersonalityCalculator pc = new PersonalityCalculator();
+	/*@Test
+	public void testFindPersonalityMethod() {
+		String temp = "4,3,5,1,2";
+		int answer[] = {4,3,5,1,2};
+		assertEquals(answer,pc.findPersonality(temp));
+		
+	}*/
+	
+	@Test
+	public void testFindYourBrainTypeMethod() {
+		String options = "1,2,3,2,1,4,5,2,3,4,5,4,3,2,3,4,5,3,2,4";
+		assertEquals("rightbrained",pc.findYourBrainType(options));
+		try {
+			if(options.contains("0"))
+				pc.findYourBrainType(options);
+			if(options.contains(null))
+				pc.findYourBrainType(options);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
